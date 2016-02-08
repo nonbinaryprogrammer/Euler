d67062544ff8ecc3512cf9b570c5e6f98dcdd7b8
balesh2 <balesh@onid.oregonstate.edu>
2016-02-08 14:34:14 -0800

    finishes problem 25 and continues math lib

---
+++ problem25.cpp
@@ -0,0 +1,30 @@
+#include "./mathlib.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/*******************************************************************************
+ * Project Euler Problem 25: 1000-digit Fibonacci number
+ * The Fibonacci sequence is defined by the recurrence relation Fn = Fn-1 +
+ * Fn-2, where F1 = 1 and F2 = 1.
+ * Correct
+ *******************************************************************************/
+
+int main() {
+  int index;
+  BigInt Fn, Fn1, Fn2;
+
+  index = 12;
+  Fn = 144;
+  Fn1 = 89;
+
+  while(Fn.length() < 1000) {
+    Fn2 = Fn1;
+    Fn1 = Fn;
+    Fn = Fn1 + Fn2;
+    index++;
+  }
+
+  printf("index: %d\n", index);
+
+  return 0;
+}
